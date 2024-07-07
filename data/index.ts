ad15c3567ed3761d2051e65622cef3e57a946d2d
AkemMensah <141195emmanuel@gmail.com>
2024-07-07 19:37:26 +0000

    added a static data for the Approach component

--- data/index.ts
+++ data/index.ts
@@ -6,9 +6,12 @@ import {
   HiMiniUserCircle,
 } from "react-icons/hi2";
 
-import ProjectImg1 from "@/public/img/project1.png";
-import ProjectImg2 from "@/public/img/project2.png";
-import ProjectImg3 from "@/public/img/project3.png";
+import ProjectImg1 from "@/public/img/bastian-riccardi-BQ9usyzHx_w-unsplash.jpg";
+import ProjectImg2 from "@/public/img/abillion-F0e3AdcwVbM-unsplash.jpg";
+import ProjectImg3 from "@/public/img/payment_app.jpg";
+import Questions from "@/public/svg/Questions-amico.svg";
+import Progress from "@/public/svg/Product iteration-amico.svg";
+import Launch from "@/public/svg/Market launch-amico.svg";
 import Logo1 from "@/public/img/logo1.png";
 import Logo2 from "@/public/img/logo2.png";
 import Logo3 from "@/public/img/logo3.png";
@@ -16,12 +19,13 @@ import Avatar1 from "@/public/img/avatar1.png";
 import Avatar2 from "@/public/img/avatar2.png";
 import Avatar3 from "@/public/img/avatar3.png";
 import Avatar4 from "@/public/img/avatar4.png";
-import Laravel from "@/public/svg/laravel.svg";
+import Ux from "@/public/svg/ui ux design.png";
 import React from "@/public/svg/react.svg";
-import Vue from "@/public/svg/vue-9.svg";
-import ViteJS from "@/public/svg/vitejs.svg";
+import Django from "@/public/svg/django.svg";
+import Html from "@/public/svg/code.png";
 import Python from "@/public/svg/python.svg";
-import Node from "@/public/svg/node.svg";
+import Js from "@/public/svg/java-script.svg";
+import { AiFillMedicineBox } from "react-icons/ai";
 
 export const navItems = [
   {
@@ -51,57 +55,116 @@ export const navItems = [
   },
 ];
 
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
 export const projectsItems = [
   {
-    title: "SAS Website",
+    title: "E-commerce Website",
     image: ProjectImg1,
   },
   {
-    title: "Social Mendia Website",
+    title: "Restaurant App",
     image: ProjectImg2,
   },
   {
-    title: "Admin Panel Website",
+    title: "Morden Payment App",
     image: ProjectImg3,
   },
 ];
 
 export const skillsItems = [
   {
-    name: "Laravel",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    logo: Laravel,
+    name: "JavaScript",
+    description:"Great website interactivity with JS",
+    logo: Js,
   },
   {
-    name: "React",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    name: "React/Nextjs",
+    description:"FrontEnd expert with React & NextJs",
     logo: React,
   },
   {
-    name: "Vue",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    logo: Vue,
-  },
-  {
-    name: "ViteJS",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    logo: ViteJS,
-  },
-  {
     name: "Python",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description:"Elegant architecture in python",
     logo: Python,
   },
   {
-    name: "Node",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    logo: Node,
+    name: "Django",
+    description:"Ardent developer with Django",
+    logo: Django,
+  },
+  {
+    name: "UX/UI",
+    description:"Seemless interface designs",
+    logo: Ux,
+  },
+  
+  {
+    name: "HTML/CSS",
+    description:"Semantic Html with flawless designs",
+    logo: Html,
   },
 ];
 
@@ -152,3 +215,26 @@ export const testimonialsItems = [
     image: Avatar4,
   },
 ];
+
+export const approachItems = [
+  {title:"Planning & Strategy",
+    des:"We'll collaborate to map out your website's goals, target audience,\
+            and key functionalities. We'll discuss things like site structure\
+          navigation, and content requirements.",
+    image: Questions,
+  },
+  {
+    title:"Development & Progress Update",
+    des:"Once we agree on the plan, I cue my lofi playlist and dive into\
+          coding. From initial sketches to polished code, I keep you updated\
+          every step of the way.",
+    image: Progress,
+  },
+  {
+    title:"Deployment & Launch",
+    des:"This is where the magic happens! Based on the approved design,\
+          I'll translate everything into functional code, building your website\
+          from the ground up.",
+    image:Launch,
+  }
+]
