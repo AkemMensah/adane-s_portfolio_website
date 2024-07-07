ab08f8edc3fd570a98cb44c1823543b686ada172
AkemMensah <141195emmanuel@gmail.com>
2024-07-07 18:58:05 +0000

    added the code for the Testimonials

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -12,8 +12,8 @@ const Testimonials = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   return (
     <section>
-      <div className="px-5 pt-14 md:container">
-        <h2 className="title text-yellow-500" data-aos="fade-down">
+      <div className="px-5 pt-14 md:container bg-gray-400">
+        <h2 className="title text-purple-500" data-aos="fade-down">
           Testimonials
         </h2>
         <h4 className="subtitle" data-aos="fade-down">
