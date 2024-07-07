a5a6a14fe55c48f6517925075262d98db929c124
AkemMensah <141195emmanuel@gmail.com>
2024-07-07 19:32:56 +0000

    added the code for the component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-
+import Image from "next/image";
+import Me from "@/public/img/Me.png";
 const Footer = () => {
   return (
     <footer className="bg-gray-950">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div className="md:flex md:justify-between">
+        <div className="md:flex md:justify-around">
           <div className="mb-6 md:mb-0">
             <a href="https://flowbite.com/" className="flex items-center">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
+              <Image
+                src={Me}
                 className="h-8 me-3"
-                alt="FlowBite Logo"
+                alt="Logo"
               />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
+                AkemMensah
               </span>
             </a>
           </div>
@@ -23,12 +24,12 @@ const Footer = () => {
                 Resources
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
+                <li className="mb-2">
                   <a href="https://flowbite.com/" className="hover:underline">
                     Flowbite
                   </a>
                 </li>
-                <li>
+                <li className="mb-2">
                   <a
                     href="https://tailwindcss.com/"
                     className="hover:underline"
@@ -36,27 +37,59 @@ const Footer = () => {
                     Tailwind CSS
                   </a>
                 </li>
+                <li className="mb-2">
+                  <a
+                    href="https://michalsnik.github.io/aos/"
+                    className="hover:underline"
+                  >
+                    AOS
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://swiperjs.com/"
+                    className="hover:underline"
+                  >
+                    Swiper Js
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                Follow us
+                Let's Connect:
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
+                <li className="mb-2">
                   <a
-                    href="https://github.com/themesberg/flowbite"
+                    href="https://github.com/AkemMensah/"
                     className="hover:underline "
                   >
                     Github
                   </a>
                 </li>
-                <li>
+                <li className="mb-2">
                   <a
-                    href="https://discord.gg/4eeurUVvTy"
+                    href="https://linkedin.com/AkemMensah"
+                    className="hover:underline"
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+                <li className="mb-2">
+                  <a
+                    href="https://discord.gg/"
                     className="hover:underline"
                   >
                     Discord
+                  </a>
+                </li>
+                <li className="mb-2">
+                  <a
+                    href="https://twiter.com/"
+                    className="hover:underline"
+                  >
+                    X
                   </a>
                 </li>
               </ul>
@@ -81,9 +114,9 @@ const Footer = () => {
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
+        <div className="sm:flex sm:items-center sm:justify-around">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 lg:flex lg:justify-center">
+            © 2024{" "}
             <a href="https://flowbite.com/" className="hover:underline">
               Flowbite™
             </a>
