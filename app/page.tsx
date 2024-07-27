4a25420c065a6cd57fabed8c066cf9db61f9c7f8
AkemMensah <141195emmanuel@gmail.com>
2024-07-27 14:29:35 +0000

    rendered the ContactAPI.js component in the pages.js file

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,18 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Navbar from "@/components/Navbar";
+import About from "@/components/About"
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skill";
 import Approach from "@/components/Approach";
 import Services from "@/components/Services";
 import Contacts from "@/components/Contacts";
+import ContactForm from "@/components/ContactForm";
 import Footer from "@/components/Footer";
 import Testimonials from "@/components/Testimonials";
 import { useEffect } from "react";
+import Head from 'next/head';
 
 
 export default function Home() {
@@ -23,16 +26,20 @@ export default function Home() {
     });
   }, []);
   return (
-    <div className="bg-grey-950" >
-      
+    <div className="portfolio" >
+      <Head>
+        <link rel="icon" href="/img/butterfly.png" type="image/png" />
+      </Head>
       < Navbar />
       < Hero />
+      < About />
       < Skills />
       < Services />
       < Approach />
       < Projects />
       < Testimonials />
       < Contacts />
+      < ContactForm />
       < Footer />
       
     </div>
